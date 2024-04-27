8e82e935a17972b1d565e32784dc34a5ba8a95c3
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-04-28 06:27:44 +0700

    add error middleware and fixing app.js

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 3030
+const morgan = require('morgan')
+const userRouter = require('./routes/userRoute.js')
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use('/api', userRouter)
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        status: 'failed',
+        errors: err.message
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        status:'failed',
+        errors:'API endpoint not found'
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Application running on port:${PORT}`);
