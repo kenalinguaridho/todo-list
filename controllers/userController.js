3c0cbfbdf17ac041f0cbde1ae6fafa89e0056dcb
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-04-28 06:26:35 +0700

    finish register function

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,46 @@ const { User } = require('../models')
 class UserController {
     static register = async (req, res) => {
         try {
-            
+
+            let { firstName, lastName, email, phone, pin, rePin } = req.body
+
+            const payload = {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                phone: phone,
+                pin: pin,
+                rePin: rePin
+            }
+
+            if (payload.pin.length != 6) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: 'pin must be 6 char length'
+                })
+            }
+
+            if (rePin !== pin) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: `pin isn't consistent`
+                })
+            }
+
+            await User.create(payload)
+
+            return res.status(201).json({
+                status: 'success',
+                message: 'user register successful'
+            })
+
         } catch (error) {
-            
+
+            return res.status(400).json({
+                status: 'failed',
+                message: 'failed while creating data'
+            })
+
         }
     }
 
@@ -13,3 +50,5 @@ class UserController {
 
     }
 }
+
+module.exports = { UserController }
