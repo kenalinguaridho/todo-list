c2943419b238b84f207d98f49da0ac92e1957532
Muhammad Ridho <muhammadridho.mail@gmail.com>
2024-04-28 09:56:27 +0700

    finish login function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,8 @@
-const { User } = require('../models')
+const 
+    { Op } = require('sequelize'),
+    { User } = require('../models'),
+    { compareSync } = require('bcryptjs'),
+    jwt = require('jsonwebtoken')
 
 class UserController {
     static register = async (req, res) => {
@@ -40,13 +44,67 @@ class UserController {
 
             return res.status(400).json({
                 status: 'failed',
-                message: 'failed while creating data'
+                message: error.errors[0].message || 'failed while creating data'
             })
 
         }
     }
 
     static login = async (req, res) => {
+        try {
+
+            let { user, pin } = req.body
+
+            const payload = {
+                user: user ?? '',
+                pin: pin ?? ''
+            }
+
+            payload.user = payload.user.toLowerCase()
+
+            const userLogin = await User.findOne({
+                where: {
+                    [Op.or]: [{ email: payload.user }, { phone: payload.user }]
+                },
+                attributes: ['id', 'pin']
+            })
+
+            if (!userLogin) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: 'user not found'
+                })
+            }
+
+            const comparePassword = compareSync(payload.pin, userLogin.dataValues.pin)
+
+            if (!comparePassword) {
+                return res.status(400).json({
+                    status: 'failed',
+                        message: `password didn't match`
+                })
+            }
+
+            const dataLogin = {
+                id: userLogin.dataValues.id,
+            }
+
+            const accessToken = jwt.sign(dataLogin, process.env.SECRET_KEY)
+
+            dataLogin.accessToken = accessToken
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'login success',
+                payload: dataLogin
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                status: "failed",
+                message: error
+            })
+        }
 
     }
 }
